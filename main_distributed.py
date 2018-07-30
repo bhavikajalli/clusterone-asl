01efee4b0fb538e2613937f36f7f883ec6bfed77
Bhavika Reddy Jalli <bhavikaj@umich.edu>
2018-07-29 17:24:08 -0700

    Update main_distributed.py

--- main_distributed.py
+++ main_distributed.py
@@ -5,7 +5,7 @@
 
 
 import numpy as np
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import os
 import tensorflow as tf
